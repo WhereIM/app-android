446ab90d91aba55c8bb2cbd6f15621ba1b57940e
Buganini Q <buganini@gmail.com>
2017-01-03 03:15:10 +0800

    google map and aws iot mqtt is working

--- app/src/main/java/im/where/whereim/CoreService.java
+++ app/src/main/java/im/where/whereim/CoreService.java
@@ -14,6 +14,25 @@ import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
+import com.amazonaws.mobileconnectors.iot.AWSIotKeystoreHelper;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttManager;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttNewMessageCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.util.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.security.KeyStore;
+
+import static com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback.AWSIotMqttClientStatus.*;
+
 public class CoreService extends Service {
     private final static String TAG = "CoreService";
     public class CoreBinder extends Binder{
@@ -25,10 +44,66 @@ public class CoreService extends Service {
     public CoreService() {
     }
 
+    private String CLIENT_ID = "xxxxx";
+    private final static String ACCOUNT_ENDPOINT_PREFIX = "a3ftvwpcurxils";
+    private final static Region REGION_ID = Region.getRegion(Regions.AP_NORTHEAST_1);
+
+    private static final String CERT_ID = "xxxxx";
+    private static final String CERT_FILE = "test1.crt";
+    private static final String PRIVATE_KEY_FILE = "test1.key";
+
+    private KeyStore keyStore;
+    private static final String KEY_STORE_NAME = "xxxxx.jks";
+    private static final String KEY_STORE_PASSWORD = "xxxxx";
+    private String keyStorePath;
+
+    private AWSIotMqttManager mqttManager;
+
     @Override
     public IBinder onBind(Intent intent) {
         startLocationService();
+
+        keyStorePath = getFilesDir().getAbsolutePath();
+
+        mqttManager = new AWSIotMqttManager(CLIENT_ID, REGION_ID, ACCOUNT_ENDPOINT_PREFIX);
+        String cert = assetsFileToString(CERT_FILE);
+        String priv = assetsFileToString(PRIVATE_KEY_FILE);
+        File keyStoreFile = new File(keyStorePath, KEY_STORE_NAME);
+        if(!keyStoreFile.exists() || !AWSIotKeystoreHelper.keystoreContainsAlias(CERT_ID, keyStorePath, KEY_STORE_NAME, KEY_STORE_PASSWORD)){
+            AWSIotKeystoreHelper.saveCertificateAndPrivateKey(CERT_ID, cert, priv, keyStorePath, KEY_STORE_NAME, KEY_STORE_PASSWORD);
+        }
+        keyStore = AWSIotKeystoreHelper.getIotKeystore(CERT_ID, keyStorePath, KEY_STORE_NAME, KEY_STORE_PASSWORD);
+        mqttManager.connect(keyStore, new AWSIotMqttClientStatusCallback() {
+            @Override
+            public void onStatusChanged(AWSIotMqttClientStatus status, Throwable throwable) {
+                Log.d(TAG, "AWSIotMqttClientStatus changed: "+status);
+                switch (status){
+                    case Connected:
+                        mqttManager.publishString("lala", "test", AWSIotMqttQos.QOS1);
+                        mqttManager.subscribeToTopic("lala", AWSIotMqttQos.QOS1, new AWSIotMqttNewMessageCallback() {
+                            @Override
+                            public void onMessageArrived(String topic, byte[] data) {
+                                Log.e(TAG, topic+" "+new String(data));
+                            }
+                        });
+                        break;
+                }
+            }
+        });
+
         return mBinder;
+    }
+
+    private String assetsFileToString(String path){
+        try {
+            InputStream json = getAssets().open(path);
+            String ret = IOUtils.toString(json);
+            json.close();
+            return ret;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private LocationManager mLocationManager;
---
+++ app/src/main/java/im/where/whereim/WhereImApplication.java
@@ -0,0 +1,10 @@
+package im.where.whereim;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by buganini on 03/01/17.
+ */
+
+public class WhereImApplication extends MultiDexApplication {
+}
