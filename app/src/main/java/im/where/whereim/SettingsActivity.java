71dc3de802aa341489a18f546208d07fedbe852f
Buganini Q <buganini@gmail.com>
2019-11-11 08:21:35 +0800

    display account info on logout button

--- app/src/main/java/im/where/whereim/SettingsActivity.java
+++ app/src/main/java/im/where/whereim/SettingsActivity.java
@@ -1,12 +1,15 @@
 package im.where.whereim;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -41,7 +44,11 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+
+        SharedPreferences settings = getSharedPreferences(Config.APP_SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
+        TextView logout = findViewById(R.id.logout);
+        logout.setText(String.format("%s (%s)", getString(R.string.logout, settings.getString(Key.NAME, null)), settings.getString(Key.PROVIDER, null)));
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new AlertDialog.Builder(SettingsActivity.this)
